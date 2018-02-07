83e09eb96eea0c4625b4edf14d170dcb11e32bb5
Renée Kooi <renee@kooi.me>
2018-02-07 17:11:37 +0100

    loading for server side rendering

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
+var parallel = require('run-parallel')
+
+var IDLE = 0
+var LOADING = 1
+var LOADED = 2
+
 module.exports = function (app) {
-  return function (load) {
-    // TODO custom loading views
-    var str = new String('<body></body>')
-    str.__encoded = true
-    return str
+  var loaders = []
+
+  function lazyRoute (load) {
+    var view = null
+    var loadingState = IDLE
+
+    function doLoad (cb, emit) {
+      loadingState = LOADING
+      load(function (err, _view) {
+        if (err) {
+          loadingState = IDLE
+          return cb(err)
+        }
+        loadingState = LOADED
+        view = _view
+      })
+    }
+    loaders.push(doLoad)
+
+    return function (state, emit) {
+      if (loadingState === IDLE) {
+        doLoad(function (err) {
+          if (err) emit('error', err)
+          else emit('render')
+        }, emit)
+      }
+      if (loadingState === LOADING) {
+        // TODO custom loading views
+        var str = new String('<body></body>') // eslint-disable-line no-new-wrappers
+        str.__encoded = true
+        return str
+      }
+      return view(state, emit)
+    }
   }
+
+  lazyRoute.all = function (cb) {
+    parallel(loaders, function (err) {
+      if (err) cb(err)
+      else cb()
+    })
+  }
+
+  return lazyRoute
 }
